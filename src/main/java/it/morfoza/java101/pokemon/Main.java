6a5253cb74d96828f2da7b387a729baf196486b9
Wojt <ciechman.ws@gmail.com>
2016-07-29 16:53:26 +0200

    Pokemon fight

--- src/main/java/it/morfoza/java101/pokemon/Main.java
+++ src/main/java/it/morfoza/java101/pokemon/Main.java
@@ -1,6 +1,5 @@
 package it.morfoza.java101.pokemon;
 
-import javax.sound.midi.Soundbank;
 
 /**
  * Created by Wojt on 2016-07-29.
@@ -9,11 +8,27 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Pokemon pokemon = new Pokemon();
+        Pokemon pokemon = new Pokemon("Pikachu", 1000, 100 );
+        Pokemon pokemon2 = new Pokemon("Charizard",2000, 200);
+        Pokemon pokemon3 = new Pokemon("Bulbasaur", 2000, 300);
 
-        String pokemonDescription = pokemon.toString();
+        System.out.println(pokemon);
+        System.out.println(pokemon2);
+        System.out.println(pokemon3);
 
-        System.out.println(pokemonDescription);
+        pokemon3.attack(pokemon);
+
+
+        System.out.println(pokemon);
+        System.out.println(pokemon2);
+        System.out.println(pokemon3);
+
+        if(pokemon.isConscious()) {
+            System.out.println("Pokemon is conscious");
+        } else {
+            System.out.println("Pokemon is unconscious");
+        }
+
 
     }
 }
--- src/main/java/it/morfoza/java101/pokemon/Pokemon.java
+++ src/main/java/it/morfoza/java101/pokemon/Pokemon.java
@@ -6,4 +6,39 @@ package it.morfoza.java101.pokemon;
 public class Pokemon {
 
 
+    private String name;
+    private long health;
+    private long combatpower;
+
+    public Pokemon(String name, long health, long combatpower) {
+
+        this.name = name;
+        this.combatpower = combatpower;
+        this.health = health;
+    }
+    public String toString() {
+        return "Pokemon: " + name + ", Health: " + health + ", Combat Power: " + combatpower;
+    }
+
+    public void takeAttack(long combatpower) {
+        health -= combatpower;
+    }
+
+
+    public boolean isConscious() {
+        if (health>0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void attack(Pokemon pokemon) {
+        if(this != pokemon) {
+            pokemon.takeAttack(combatpower);
+
+            System.out.println(pokemon + " gets attacked for " + combatpower + " by ?");
+        }
+    }
 }
